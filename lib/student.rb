25e51713c51067006797b33b080242ab0d74c8b9
Annia Forney <anniaforney@gmail.com>
2020-07-23 17:59:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,16 +31,38 @@ def self.drop_table
 end
 
 def save
+  if self.id
+   self.update
+ else
+   sql = <<-SQL
+     INSERT INTO students (name, grade)
+     VALUES (?, ?)
+   SQL
+   
+   DB[:conn].execute(sql, self.name, self.grade)
+   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+ end
 end
 
-def self.create
+def self.create(name, grade)
+  student = Student.new(name, grade)
+  student.save
+  student
 end
 
-def self.new_from_db
+def self.new_from_db(row)
+  id = row[0]
+  name = row[1]
+  grade = row[2]
+  self.new(id, name, grade)
 end
 
 def self.find_by_name
 end
 
+def update
+  sql = "UPDATE songs SET name = ?, grade = ? WHERE id = ?"
+  DB[:conn].execute(sql, self.name, self.grade, self.id)
+end
 
 end
