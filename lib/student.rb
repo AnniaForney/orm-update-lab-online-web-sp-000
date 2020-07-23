ca69b87d7d8f9ea64df4d5791e17bebd31dd6818
Annia Forney <anniaforney@gmail.com>
2020-07-23 18:10:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,6 @@ end
 def self.find_by_name(name)
    sql = "SELECT * FROM students WHERE name = ?"
    result = DB[:conn].execute(sql, name)[0]
-   Self.new(result[0], result[1], result[2])
  end
 end
 
